1c58f06cd22e0a10618640cd7f709d9e572682f1
Kamlesh Shah <shah.kamlesh.v@gmail.com>
2022-05-03 04:41:38 +0530

    Spring boot project initialised and executed.

--- src/main/java/in/parthinfo/springboot/customerledger/controller/HomeController.java
+++ src/main/java/in/parthinfo/springboot/customerledger/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -12,9 +13,10 @@ import javax.servlet.http.HttpSession;
 public class HomeController {
 
     @RequestMapping("home")
-    public String home(@RequestParam("name") String myName, HttpSession session){
-
-        session.setAttribute("name", myName);
-        return "home";
+    public ModelAndView home(@RequestParam("name") String myName){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", myName);
+        mv.setViewName("home");
+        return mv;
     }
 }
